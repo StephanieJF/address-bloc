a456ad57fd77cff66311d3cb40bd5d89cd1ae1b1
Stephanie Fischler <sjfischler@gmail.com>
2017-12-27 14:26:50 -0500

    Complete assignment 4: models

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -18,24 +18,41 @@ RSpec.describe AddressBook do
         end
     end
 
-describe "#add_entry" do
-    it "adds only one entry to the address book" do
-        book = AddressBook.new
-        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+    describe "#add_entry" do
+        it "adds only one entry to the address book" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
 
-        expect(book.entries.size).to eq(1)
-    end
+            expect(book.entries.size).to eq(1)
+        end
     
-    it "adds the correct information to entries" do
-        book = AddressBook.new
-        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
-        new_entry = book.entries[0]
+        it "adds the correct information to entries" do
+            book = AddressBook.new
+            book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            new_entry = book.entries[0]
         
-        expect(new_entry.name).to eq('Ada Lovelace')
-        expect(new_entry.phone_number).to eq('010.012.1815')
-        expect(new_entry.email).to eq('augusta.king@lovelace.com')
+            expect(new_entry.name).to eq('Ada Lovelace')
+            expect(new_entry.phone_number).to eq('010.012.1815')
+            expect(new_entry.email).to eq('augusta.king@lovelace.com')
+            end
+        end
+
+    describe "#remove_entry" do
+        it "removes an entry from the address book" do
+            book = AddressBook.new
+            book.add_entry('Nina Fischler', '954.123.4567', 'nina@sparky.com')
+            
+            name = 'Ada Lovelace'
+            phone_number = '010.012.1815'
+            email_address = 'augusta.king@lovelace.com'
+            book.add_entry(name, phone_number, email_address)
+            
+            expect(book.entries.size).to eq(2)
+            book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+            
+            expect(book.entries.size).to eq(1)
+            expect(book.entries.first.name).to eq('Nina Fischler')
         end
     end
-
 end
 

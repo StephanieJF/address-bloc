13a1232ec293d61b62bb31582877d6b72a6fab82
Stephanie Fischler <sjfischler@gmail.com>
2018-01-03 10:21:20 -0500

    Update branch

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,13 +1,14 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
-    attr_reader :entries
-    
-    def initialize
-        @entries = []
-    end
-
-	def add_entry(name, phone_number, email)
+	attr_reader :entries
+	
+	def initialize
+		@entries = []
+	end
+	
+	def add_entry(name, phone, email)
 		index = 0
 		entries.each do |entry|
 			
@@ -15,12 +16,17 @@ class AddressBook
 				break
 			end
 			index+=1
-	end
-		
-		entries.insert(index, Entry.new(name, phone_number, email))
 		end
-
-	def binary_search(name)
+		
+		entries.insert(index, Entry.new(name, phone, email))
 	end
-
+	
+	def import_from_csv(file_name)
+		csv_text = File.read(file_name)
+		csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+		csv.each do |row|
+			row_hash = row.to_hash
+			add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+		end
+	end
 end
